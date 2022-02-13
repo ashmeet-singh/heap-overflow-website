2a041b6f446e0b2d441ba3e2a8adf6716d8793eb
ashmeet-singh <76838543+ashmeet-singh@users.noreply.github.com>
2022-02-14 02:25:58 +0530

    Formated code

--- js/firebase_module.js
+++ js/firebase_module.js
@@ -1,19 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    onAuthStateChanged,
-    signOut,
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
 } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDkLn5SVqfVwLM9_ZC7PznuJ2VSTOyIbT0",
-    authDomain: "heap-overflow-s92n67.firebaseapp.com",
-    projectId: "heap-overflow-s92n67",
-    storageBucket: "heap-overflow-s92n67.appspot.com",
-    messagingSenderId: "783709822291",
-    appId: "1:783709822291:web:92a9a6826e8a566179d5a6",
+  apiKey: "AIzaSyDkLn5SVqfVwLM9_ZC7PznuJ2VSTOyIbT0",
+  authDomain: "heap-overflow-s92n67.firebaseapp.com",
+  projectId: "heap-overflow-s92n67",
+  storageBucket: "heap-overflow-s92n67.appspot.com",
+  messagingSenderId: "783709822291",
+  appId: "1:783709822291:web:92a9a6826e8a566179d5a6",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -21,57 +21,57 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 
 onAuthStateChanged(auth, (user) => {
-    if (user) {
-        // User is signed in, see docs for a list of available properties
-        const uid = user.uid;
-        console.log(user);
-    } else {
-        // User is signed out
-        console.log("User signed out");
-    }
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    const uid = user.uid;
+    console.log(user);
+  } else {
+    // User is signed out
+    console.log("User signed out");
+  }
 });
 
 QS("#SignUpButton").addEventListener("click", function (e) {
-    createUserWithEmailAndPassword(
-        auth,
-        QS("#UsernameInput").value,
-        QS("#PasswordInput").value
-    )
-        .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ..
-        });
+  createUserWithEmailAndPassword(
+    auth,
+    QS("#UsernameInput").value,
+    QS("#PasswordInput").value
+  )
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
 });
 
 QS("#SignInButton").addEventListener("click", function (e) {
-    signInWithEmailAndPassword(
-        auth,
-        QS("#UsernameInput").value,
-        QS("#PasswordInput").value
-    )
-        .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-        });
+  signInWithEmailAndPassword(
+    auth,
+    QS("#UsernameInput").value,
+    QS("#PasswordInput").value
+  )
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
 });
 
 QS("#SignOutButton").addEventListener("click", function (e) {
-    signOut(auth)
-        .then(() => {
-            // Sign-out successful.
-        })
-        .catch((error) => {
-            // An error happened.
-        });
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
+    });
 });
--- js/global.js
+++ js/global.js
@@ -1,13 +1,15 @@
 function QS(selectors, baseElement) {
-    if (baseElement === undefined) { baseElement = document.body; }
-    element = baseElement.querySelector(selectors);
-    return element;
+  if (baseElement === undefined) {
+    baseElement = document.body;
+  }
+  element = baseElement.querySelector(selectors);
+  return element;
 }
 
 function QSA(selectors, parentNode) {
-    if (parentNode === undefined) { parentNode = document.body; }
-    elementList = parentNode.querySelectorAll(selectors);
-    return elementList;
+  if (parentNode === undefined) {
+    parentNode = document.body;
+  }
+  elementList = parentNode.querySelectorAll(selectors);
+  return elementList;
 }
-
-
